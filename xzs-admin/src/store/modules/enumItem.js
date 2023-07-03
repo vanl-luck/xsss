cff17cbb2ed3a77437b0c217b0b9237209aa1aea
vanl-luck <414599861@qq.com>
2023-07-03 13:11:05 +0800

    更改年纪

--- xzs-admin/src/store/modules/enumItem.js
+++ xzs-admin/src/store/modules/enumItem.js
@@ -3,9 +3,9 @@ const state = {
   user: {
     sexEnum: [{ key: 1, value: '男' }, { key: 2, value: '女' }],
     statusEnum: [{ key: 1, value: '启用' }, { key: 2, value: '禁用' }],
-    levelEnum: [{ key: 1, value: '一年级' }, { key: 2, value: '二年级' }, { key: 3, value: '三年级' }, { key: 4, value: '四年级' }, { key: 5, value: '五年级' }, { key: 6, value: '六年级' },
-      { key: 7, value: '初一' }, { key: 8, value: '初二' }, { key: 9, value: '初三' },
-      { key: 10, value: '高一' }, { key: 11, value: '高二' }, { key: 12, value: '高三' }],
+    levelEnum: [{ key: 1, value: '专业一' }, { key: 2, value: '专业二' }, { key: 3, value: '专业三' }, { key: 4, value: '专业四' }, { key: 5, value: '专业五' }, { key: 6, value: '专业六' },
+      { key: 7, value: '专业七' }, { key: 8, value: '专业八' }, { key: 9, value: '专业九' },
+      { key: 10, value: '高级一' }, { key: 11, value: '高级二' }, { key: 12, value: '高级三' }],
     roleEnum: [{ key: 1, value: '学生' }, { key: 2, value: '教师' }, { key: 3, value: '管理员' }],
     statusTag: [{ key: 1, value: 'success' }, { key: 2, value: 'danger' }],
     statusBtn: [{ key: 1, value: '禁用' }, { key: 2, value: '启用' }]
