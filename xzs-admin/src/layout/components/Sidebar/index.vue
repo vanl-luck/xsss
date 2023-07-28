80c762f41fe1cd9e641eea259e2cbcd8f8c80b27
huxingtao <36324488+ibule@users.noreply.github.com>
2023-07-28 23:25:56 +0800

    add

--- xzs-admin/src/layout/components/Sidebar/index.vue
+++ xzs-admin/src/layout/components/Sidebar/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="has-logo">
-    <logo  :collapse="isCollapse" />
+    <logo :collapse="isCollapse" />
     <el-scrollbar wrap-class="scrollbar-wrapper">
       <el-menu
         :default-active="activeMenu"
@@ -12,40 +12,66 @@
         :collapse-transition="false"
         mode="vertical"
       >
-        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+        <sidebar-item
+          v-for="route in filteredRoutes"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        />
       </el-menu>
     </el-scrollbar>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Logo from './Logo'
-import SidebarItem from './SidebarItem'
-import variables from '@/styles/variables.scss'
+import { mapGetters } from "vuex";
+import Logo from "./Logo";
+import SidebarItem from "./SidebarItem";
+import variables from "@/styles/variables.scss";
+import Cookies from "js-cookie";
 
 export default {
   components: { SidebarItem, Logo },
   computed: {
-    ...mapGetters([
-      'sidebar',
-      'routes'
-    ]),
-    activeMenu () {
-      const route = this.$route
-      const { meta, path } = route
+    ...mapGetters(["sidebar", "routes", "userName"]),
+    filteredRoutes() {
+      // const userName = Cookies.get("adminUserName");
+      console.log("userName", this.userName);
+
+      // debugger;
+
+      if (this.userName == "admin") {
+        console.log("this.routes", this.routes);
+        return this.routes;
+      }
+
+      const filterroutes = [];
+      this.routes.forEach((item) => {
+        if (item.name == "UserPage") {
+          item.children = item.children.filter(
+            (route) => route.name !== "UserAdminPageList"
+          );
+        }
+        filterroutes.push(item);
+      });
+      return filterroutes;
+      // return userName == "admin" ? this.routes : filterroutes;
+    },
+    activeMenu() {
+      const route = this.$route;
+      const { meta, path } = route;
       // if set path, the sidebar will highlight the path you set
       if (meta.activeMenu) {
-        return meta.activeMenu
+        return meta.activeMenu;
       }
-      return path
+      return path;
     },
-    variables () {
-      return variables
+    variables() {
+      return variables;
     },
-    isCollapse () {
-      return !this.sidebar.opened
-    }
-  }
-}
+    isCollapse() {
+      return !this.sidebar.opened;
+    },
+  },
+};
 </script>
